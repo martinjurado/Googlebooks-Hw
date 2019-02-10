adeaf12fd0cad7d249ed67ceb9d8427d28280e5e
unknown <juradomartinbenedict@gmail.com>
2019-02-10 13:10:52 -0500

    made it less boring

--- client/src/components/BookDetails/index.js
+++ client/src/components/BookDetails/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import "./style.css"
 
 
 // // RecipeList renders a bootstrap list item
@@ -10,12 +11,12 @@ import React from "react";
 const BookItems = (props) => {
     
     return (
-        <div className="text-center">
-            <img alt={props.title} src={props.thumbnail} />
-            <h4>{props.title}</h4>
+        <div className="text-center containerapi">
+            <img alt={props.title} src={props.thumbnail} className="apipic" />
+            <h5>{props.title}</h5>
             <h6>{props.authors}</h6>
-            <p>{props.description}</p>
-            <a href={props.href} target="_blank" rel="noopener noreferrer">Go to Book</a>
+            <p className="bookdesc">{props.description}</p>
+            <a href={props.href} target="_blank" rel="noopener noreferrer" className="link">Go to Book</a>
             <br></br>
         </div>
     )
